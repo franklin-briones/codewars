9591766a0fdccfaad86eac75841378aed29ddf22
Franklin Briones <franklindbriones@gmail.com>
2022-04-16 17:11:48 -0500

    doing my first 8 kata for interview practice

--- 8 kyu/8kyu.js
+++ 8 kyu/8kyu.js
@@ -1,10 +1,48 @@
-// Even or Odd
+//Even or odd
 
-function even_or_odd(number) {
-    if (number % 2===0) {
-        return 'Even';
-    }
-    else {
-        return 'Odd';
-    }
+// //Using simple if statements
+// function even_or_odd(number) {
+//     if (number%2===0) {
+//         return 'Even';
+//     }
+//     else {
+//         return 'Odd';
+//     }
+// }
+// //Using ? operator
+// function evenOrOddQuestion(number)  {
+//     return number%2 ? 'Even':'Odd'
+// }
+
+//String to Number
+var stringToNumber = function(str) {
+    //Transform string into number where any input is a string of numbers
+    let num=Number(str)
+    return parseInt(str)
+
+    return num;
 }
+
+//Using the Unary operator
+var stringToNumUnary = function(str) {
+    return +str;
+}
+//Return time since midnight in milliseconds
+//h - 0<=h<=23
+//m - 0<=h<=59
+//s - 0<=h<=59
+
+//Example: h=0,m=1,s=2
+function past(h,m,s){
+    //Find number of hours since midnight and convert to seconds
+    let secondsH=h*60*60*1000;
+    let secondsM=m*60*1000;
+    let secondsS=s*1000;
+    let total=secondsH+secondsM+secondsS;
+    return total;
+}
+//Less LOC to return same
+function pastLOC(h,m,s) {
+    return (h*3600+m*60+s)*1000;
+}
+
